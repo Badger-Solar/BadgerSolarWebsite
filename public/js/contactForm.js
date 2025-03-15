56666aef29fe961dfb99bea969ed3a78f1d21c17
SuperSaurabh <12saurabh.abhyankar@gmail.com>
2025-03-15 16:59:57 -0500

    images

--- public/js/contactForm.js
+++ public/js/contactForm.js
@@ -3,6 +3,11 @@ document
   .addEventListener("submit", async (e) => {
     e.preventDefault();
 
+    const loader = document.getElementById("loader");
+    const statusMessage = document.getElementById("statusMessage");
+    statusMessage.innerText = "";
+    loader.style.display = "block";
+
     const demoCORSurl = "https://cors-anywhere.herokuapp.com/";
     const teamsWebhookURL =
       "https://uwprod.webhook.office.com/webhookb2/6a03ad30-c770-4955-b28f-763caa75c448@2ca68321-0eda-4908-88b2-424a8cb4b0f9/IncomingWebhook/f51fced44aca4c2ab2b0def2bbce7276/2cb6e2a1-9847-4d6c-842b-0959445ea61f/V21VW-MWY99_ZanWS5rzByoaHzc6G8kOZBxeDtys43ays1";
@@ -20,14 +25,12 @@ document
       });
 
       if (response.ok) {
-        document.getElementById("statusMessage").innerText =
-          "Message Sent Successfully!";
+        statusMessage.innerText = "Message Sent Successfully!";
+        loader.style.display = "none";
       } else {
-        document.getElementById("statusMessage").innerText =
-          "Error Sending Message, please email us.";
+        statusMessage.innerText = "Error Sending Message, please email us.";
       }
     } catch (error) {
-      document.getElementById("statusMessage").innerText =
-        "Microsoft Teams Error, please email us.";
+      statusMessage.innerText = "Microsoft Teams Error, please email us.";
     }
   });
