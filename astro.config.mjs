bb39f8aefe61faa22c5a5adf43a061b86e170d4c
SuperSaurabh <12saurabh.abhyankar@gmail.com>
2025-09-10 15:04:09 -0500

    sanity enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sanity from "@sanity/astro";
+
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://badgersolarracing.org',
+  site: 'https://badgersolarracing.org',
+  integrations: [sanity({
+    projectId: "3r1aka6c",
+    dataset: "production",
+    useCdn: false, // for static builds
+  })],
 });
